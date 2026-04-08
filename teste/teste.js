9b03ce648289c8e2f75ccdf6bc7a4539fd2f8f1d
gabriellycama <gabrielly.camargo.pereira@escola.pr.gov.br>
2026-04-08 11:52:20 -0300

    Add files via upload

---
+++ teste/teste.js
@@ -0,0 +1,42 @@
+// 1 exiba a mensagem "ola mundo" no console
+
+console.log("ola mundo");
+
+
+// 2. crie uma variavel com seu nome  e exiba o valor no console
+
+let nome = "camile";
+console.log(nome);
+
+//3. some dois numeros e exiba e resultado
+
+let c =1;
+let h =4;
+console.log(c+h);
+
+
+let soma = 10 + 3;
+
+console.log(soma);
+
+//4.subtraia dois numero e exiba o valor
+
+let sub = 10 -3;
+console.log(sub);
+
+//5 descubra o resto da divisao de um numero por outro
+let restos = 10%3;
+console.log(restos)
+
+
+//6. crie uma variavel boleana(true false) e exiba seu valor.
+
+let bola = true;
+console.log(bola);
+
+//7. verifique se um numero e maior q outro 
+
+let maiorque = 18>11;
+console.log(maiorque);
+
+//8
