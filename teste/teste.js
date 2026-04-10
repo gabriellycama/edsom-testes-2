497b732bcd1c882816c4062f09408bf73eb89939
gabriellycama <gabrielly.camargo.pereira@escola.pr.gov.br>
2026-04-10 08:20:20 -0300

    Add files via upload

--- teste/teste.js
+++ teste/teste.js
@@ -1,3 +1,7 @@
+//gabrielly
+
+
+
 // 1 exiba a mensagem "ola mundo" no console
 
 console.log("ola mundo");
@@ -39,4 +43,89 @@ console.log(bola);
 let maiorque = 18>11;
 console.log(maiorque);
 
-//8
+//8.crie duas variaveis com algum valor e exiba a 
+
+let sola=2;
+
+let solo=5;
+
+console.log(sola,solo);
+// professor
+let num1 = "teste";
+let num2 ="teste2";
+
+console.log(`teste ${num1} ${num2}`);
+
+//9.descubra o tipo de uma variavel;
+let valor = 100;
+console.log(typeof valor); // typeof mostra o tipo da  variavel
+
+//10 converta numero para string
+
+let valors= 20;
+console.log(String(valors));
+
+//11. string para numero
+let texto = "123";
+console.log(Number(texto));
+
+
+//12 exiba no console numeros de 0 a 10.
+
+//for(let i = 0; i<=10; i++){
+ //   console.log(i);
+
+//}
+
+
+// exiba apenas numeros pares ate 20.
+
+for(let i = 1; i<=20; i+=2){
+    
+    console.log(i);
+}
+
+//14 mostre no console de 10 ate 1 
+
+
+for(let i =10; i>1; i--){
+    console.log(i);
+}
+
+// 15. some numeros de 1 a 5.
+let somaloop =0;
+for(let i = 1; i<=5; i++){
+    somaloop+=i;
+    console.log("valor de i"+i+"valor de somaloop"+somaloop);
+}
+console.log(somaloop);
+
+//16. faça a  tabuada do 3.
+
+//for(let i =1; i<=10; i++){
+   // console.log("3 x" +i+"="+3*i);
+//}
+
+//17. conte quantos numeros sao maiores que 5
+
+let lista=[2,6,8,1,3];
+let cont = 0;
+for(let i=0; i<lista.length;i++){
+    if(lista[i]>5)cont++;
+}
+console.log(cont); 
+
+
+//18.percorra a string letra por letra
+
+let palavra="JFRS"
+
+
+//19.pare um loop quando encontrar o numero 7 de 0 a 10 
+
+for(let i = 0; i<=10; i++){
+    
+    if(i===7) break;
+        
+    console.log(i);
+}
